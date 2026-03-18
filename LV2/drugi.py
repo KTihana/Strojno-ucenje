25eaf7b01bc863caadd7f32a1e61b6724ed04176
Tihana <tihanak@pm.me>
2026-03-18 22:36:06 +0100

    Update drugi.py

--- LV2/drugi.py
+++ LV2/drugi.py
@@ -1,3 +1,13 @@
+'''
+U direktoriju PSU_LV/LV2/ nalazi se datoteka mtcars.csv koja sadrži različita mjerenja provedena na 32 automobila (modeli 1973-74). Opis pojedinih varijabli nalazi se u datoteci mtcars_info.txt.
+a) Učitajte datoteku mtcars.csv pomoću:
+data = np.loadtxt(open("mtcars.csv", "rb"), usecols=(1,2,3,4,5,6), delimiter=",", skiprows=1)
+b) Prikažite ovisnost potrošnje automobila (mpg) o konjskim snagama (hp) pomoću naredbe matplotlib.pyplot.scatter.
+c) Na istom grafu prikažite i informaciju o težini pojedinog vozila (npr. veličina točkice neka bude u skladu sa težinom wt).
+d) Izračunajte minimalne, maksimalne i srednje vrijednosti potrošnje (mpg) automobila.
+e) Ponovite zadatak pod d), ali samo za automobile sa 6 cilindara (cyl).
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
  
