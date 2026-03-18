a36412804e996adf5f77a0e98869c84d67018860
Tihana <tihanak@pm.me>
2026-03-18 22:51:00 +0100

    Update cetvrti.py

--- LV2/cetvrti.py
+++ LV2/cetvrti.py
@@ -1,3 +1,11 @@
+'''
+Napišite funkciju koja kao povratnu vrijednost daje sliku (polje) sa crno bijelim kvadratima jednake dimenzije koji se naizmjenično pojavljuju (vidi primjer slike ispod). 
+Funkcija kao argumente prima veličinu kvadrata u pikselima, broj kvadrata po visini i broj kvadrata po širini slike. 
+Za realizaciju ove funkcije koristite numpy funkcije zeros i ones kako biste kreirali crna i bijela polja. 
+Kako bise ih složili u odgovarajući oblik koristite numpy funkcije hstack i vstack. Za prikaz grayscale slike koristite naredbu:
+plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
